ace4559c2c631f886f38a9cb429c2d43c97925e0
Tibi <bajortibor02@gmail.com>
2025-05-15 01:00:26 +0200

    Update main.c

--- main.c
+++ main.c
@@ -2,14 +2,22 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdint.h>
+#include "xil_printf.h"
+#include "xbasic_types.h"
+#include "xparameters.h"
+#include "xuartps_hw.h" // XUARTPS_FIFO_OFFSET
+#define UART_BASEADDR XPAR_XUARTPS_0_BASEADDR //supposedly the base address or, _1_ for using second UART connection
+#define is_valid(data) ((data & (1<<8)) > 0)
 /*
 TODO:
 do something about different file types
-UART communication to FPGA board
+step directions
+maybe feedback from FPGA
 */
 const int STEP_PER_MM_X = 1;
 const int STEP_PER_MM_Y = 1;
 const int STEP_PER_MM_Z = 1;
+//ports:
 #define FLAG_X (1 << 0)
 #define FLAG_Y (1 << 1)
 #define FLAG_Z (1 << 2)
@@ -27,7 +35,8 @@ typedef enum{
     //G90 = 90, //absolute positioning
     G28 = 28, //autohome, base position
     G00 = 0, //rapid movement, move to position at max speed
-    G01 = 1 //Move to position with feed rate
+    G01 = 1,  //Move to position with feed rate
+    M30 = 30,  //stop program
 }GcodeType;
 
 typedef struct{
@@ -102,6 +111,9 @@ void steps(const GcodeCommand* cmd, BinaryFlags* flags)
     flags->feedrate = cmd->has_f ? cmd->feedrate : 0;
 }
 
+
+
+
 int main()
 {
     GcodeCommand cmd;
@@ -109,6 +121,7 @@ int main()
     char line[128];
     char* filepath = "test.gcode";
     FILE* fp = fopen(filepath, "r");
+    int last_x = -1, last_y = -1, last_z = -1, last_feedrate = -1; //feedrate probably not needed but its there just in case
     if(!fp)
     {
         perror("File can't be opened");
@@ -119,6 +132,24 @@ int main()
         if(line[0] == ';' || line[0] == '\n') continue;
         parser(line,&cmd);
         steps(&cmd,&flags);
+
+        //checking if any coordinates changed so we don't send the same data:
+        if((cmd.has_x && cmd.x != last_x) || (cmd.has_y && cmd.y != last_y) || (cmd.has_z && cmd.z != last_z) || (cmd.has_f && cmd.feedrate != last_feedrate))
+        {
+            last_x = cmd.has_x ? cmd.x : last_x;
+            last_y = cmd.has_y ? cmd.y : last_y;
+            last_z = cmd.has_z ? cmd.z : last_z;
+            last_feedrate = cmd.has_f ? cmd.feedrate : last_feedrate;
+
+            uint8_t* ptr = (uint8_t)&flags;
+
+            for(int i = 0; i < sizeof(BinaryFlags); i++)
+            {
+                while(XUartPs_IsTransmitFull(UART_BASEADDR));
+                XUartPs_WriteReg(UART_BASEADDR, XUARTPS_FIFO_OFFSET, ptr[i]); //sending raw bytes directly not the memory-mapped thing
+            }
+
+        }
         /* testing
         printf("parsed: G%02d", cmd.command);
         if(cmd.has_x) printf(" x=%d ",cmd.x);
